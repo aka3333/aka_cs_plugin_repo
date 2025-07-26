4cffb03500b29706b778cca09fdba050d430097d
aka3333 <adilkahraman89@gmail.com>
2025-07-26 03:15:50 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,9 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
 rootProject.name = "aka_cs_plugin_repo"
 include(":aka_cs")
