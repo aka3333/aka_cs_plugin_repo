ced66bcf38efe56d4d1fe6c25f1cd62566ee0b2b
aka3333 <adilkahraman89@gmail.com>
2025-07-26 03:16:31 +0300

    Update AkaCsPlugin.kt

--- aka_cs/src/main/kotlin/aka_cs/AkaCsPlugin.kt
+++ aka_cs/src/main/kotlin/aka_cs/AkaCsPlugin.kt
@@ -1,14 +1,13 @@
 package aka_cs
 
+import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.MainAPI
-import com.lagradost.cloudstream3.SearchResponse
 
 class AkaCsPlugin : MainAPI() {
     override var mainUrl = "https://www.filmmodu.nl/"
     override var name = "AKA CS"
 
     override suspend fun search(query: String): List<SearchResponse> {
-        // Basit boş liste döndürüyor, geliştirebilirsin
-        return emptyList()
+        return emptyList() // Şimdilik boş, sonra içerik eklersin
     }
 }
