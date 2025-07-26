ffe798f014dedd7f67f81028eeb3e2acf9d65451
aka3333 <adilkahraman89@gmail.com>
2025-07-26 03:20:04 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,14 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://repo.maven.apache.org/maven2") }
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        maven { url = uri("https://jitpack.io") }
+        mavenCentral()
     }
 }
 
