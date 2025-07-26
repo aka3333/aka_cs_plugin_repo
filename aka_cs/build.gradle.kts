9a3a93b82d330747cd7a59329cf158f46724b2f1
aka3333 <adilkahraman89@gmail.com>
2025-07-26 03:46:53 +0300

    Update build.gradle.kts

--- aka_cs/build.gradle.kts
+++ aka_cs/build.gradle.kts
@@ -1,23 +1,10 @@
-// Üst kısma bu blok EKLENDİ
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.3.2") // Android Gradle Plugin
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22") // Kotlin Plugin
-    }
-}
-
 plugins {
-    id("com.android.library") // Artık bulunabilecek
-    id("org.jetbrains.kotlin.android")
-    id("com.lagradost.cloudstream3") version "4.3.4" // CloudStream eklentisi
+    id("com.android.library") version "8.3.2" // Güncel AGP versiyonu
+    id("org.jetbrains.kotlin.android") version "1.9.22"
 }
 
 android {
-    namespace = "com.example.aka_cs"
+    namespace = "com.example.aka_cs" // BU SATIR KRİTİK!
     compileSdk = 34
 
     defaultConfig {
@@ -25,10 +12,13 @@ android {
         targetSdk = 34
     }
 
-    // ... (diğer android ayarları aynı)
-}
-
-dependencies {
-    implementation("com.github.Lagradost:CloudStream-3:4.3.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
 }
